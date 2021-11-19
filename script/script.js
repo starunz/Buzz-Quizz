87be2b19d18bdce0729a6800223a5b9bf658cf86
bruna <busouza01@gmail.com>
2021-11-19 05:34:21 -0500

    criado o objeto para salvar minhas informações e porteriormente validar essas informações

--- script/script.js
+++ script/script.js
@@ -2,23 +2,42 @@ const URL = 'https://mock-api.driven.com.br/api/v4/buzzquizz'; //essa parte da u
 const Container = document.querySelector('.container'); //para facilitar a vida, a gente pode usar 1 unica main para todas telas
 //já que vamos criar dinâmicamente, economiza css, html e tempo de ficar procurando :v mas cuidado nos fechamentos das divs :v
 
+
+// ------ tela 03 ------ 
+
+let quizzInfo = {} // guardar as informações que eu preciso em um objeto para validar os campos
 function basicQuizzInformation() {
+    quizzInfo = {
+        title: '',
+        image: '',
+        numberQuestions: 0,
+        numberLevels: 0
+    }
+
     Container.innerHTML = `
     <div class="create-quiz">
         <div class="title">Comece pelo começo</div>
         <div class="creating">
-            <input type="text" class="titulo" placeholder="Título do seu quizz">
+            <input type="text" class="title" placeholder="Título do seu quizz">
             <input type="text" class="url" placeholder="URL da imagem do seu quizz">
-            <input type="number" class="quantidade-perguntas" placeholder="Quantidade de perguntas do quizz">
-            <input type="number" class="quantidade-niveis" placeholder="Quantidade de níveis do quizz">
+            <input type="number" class="number-questions" placeholder="Quantidade de perguntas do quizz">
+            <input type="number" class="number-levels" placeholder="Quantidade de níveis do quizz">
         </div>
         <button class="next" onclick="createQuizzQuestions()">Prosseguir pra criar perguntas</button>
     </div>
     `
 }
 
+function saveValuesBasicQuizzInformation() {
+    const title = document.querySelector(".creating .title").value;
+    const image = document.querySelector(".creating .url").value;
+    const numberQuestions = document.querySelector(".creating .number-questions").value;
+    const numberLevels = document.querySelector(".creating .number-levels").value;
+}
+
 function validateOfBasicQuizzInfo() {
     // aqui ficará as validações 
+    // antes disso preciso salvar o valor dos inputs
 }
 
 function createQuizzQuestions() {
