8ec5a2e587fb4c24edbdda852cedd865a3626529
bruna <busouza01@gmail.com>
2021-11-19 05:10:30 -0500

    criada funções de renderização dinâmica e de validação da tela de informações básicas de criação de um quizz

---
+++ script/script.js
@@ -0,0 +1,3 @@
+const URL = 'https://mock-api.driven.com.br/api/v4/buzzquizz'; //essa parte da url é sempre a mesma o que muda é a rota.
+const Container = document.querySelector('.container'); //para facilitar a vida, a gente pode usar 1 unica main para todas telas
+//já que vamos criar dinâmicamente, economiza css, html e tempo de ficar procurando :v 
