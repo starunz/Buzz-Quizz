9004bbaac895955e5e5466d80819519b939eaf1f
LuSntgo <lu.sntgo@gmail.com>
2021-11-19 22:08:41 -0300

    Exibiçao de quizzes em formato retangular

--- script/script.js
+++ script/script.js
@@ -1,1 +1,27 @@
-alert('tá fufando 1');
+
+function sucess(response){
+    const quizzes = response.data;
+    console.log(quizzes)
+    const ulQuizzes = document.querySelector(".list-quizz-server");
+    ulQuizzes.innerHTML = '';
+
+       for (let i = 0; i < quizzes.length; i++){
+            ulQuizzes.innerHTML += ` <li>
+            <div class="quizz-header">
+            
+              <strong class="quizz-title">${quizzes[i].title}</strong>
+            </div>
+            <div class="quizz-question">
+            <div class="layer">
+              <img class="img" src="${quizzes[i].image}" width="340" height="181">     
+            </div>
+            </div>
+            </li>`
+        }
+}
+const promess = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+
+promess.then(sucess);
+
+
+//button
